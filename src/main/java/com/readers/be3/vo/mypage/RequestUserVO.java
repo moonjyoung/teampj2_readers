353edd17b358b612ba8a6a2bef6989448bdaa4a8
dev <moonjyoung1214@gmail.com>
2023-02-21 18:09:33 +0900

    moonjyoung 230221 userSeq -> uiSeq

--- src/main/java/com/readers/be3/vo/mypage/RequestUserVO.java
+++ src/main/java/com/readers/be3/vo/mypage/RequestUserVO.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class RequestUserVO {
     @Schema(description = "유저번호")
-    private Long userSeq;
+    private Long uiSeq;
     @Schema(description = "상태", example = "true")  
     private boolean status;
     @Schema(description = "메세지", example = "인증에 성공했습니다")  
